c745c2460d6b19c9a73e96a46235119b35c2f4cc
kukita <ku.k669500@gmail.com>
2019-08-03 18:31:30 +0900

    Swipeアクションを追加

--- app/src/main/java/com/example/kktyu/recycleviewtest/MainActivity.kt
+++ app/src/main/java/com/example/kktyu/recycleviewtest/MainActivity.kt
@@ -1,8 +1,9 @@
 package com.example.kktyu.recycleviewtest
 
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
-import android.support.v7.app.AppCompatActivity
-import android.support.v7.widget.LinearLayoutManager
 import android.util.Log
 import android.view.View
 import android.widget.Toast
@@ -13,25 +14,34 @@ import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
+    var dataList = mutableListOf<RowModel>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
-        Log.d("Life Cycle","onCreate")
+        Log.d("Life Cycle", "onCreate")
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         val recyclerView = recycler_list
-        val adapter = ViewAdapter(createDataList(), object : ViewAdapter.ListListener {
+
+        dataList = createDataList()
+
+        val adapter = ViewAdapter(dataList, object : ViewAdapter.ListListener {
             override fun onClickRow(tappedView: View, rowModel: RowModel) {
-                Toast.makeText(applicationContext, rowModel.title,Toast.LENGTH_LONG).show()
+                Toast.makeText(applicationContext, rowModel.title, Toast.LENGTH_LONG).show()
             }
         })
 
         recyclerView.setHasFixedSize(true)
-        recyclerView.layoutManager = LinearLayoutManager(applicationContext)
+        recyclerView.layoutManager =
+            androidx.recyclerview.widget.LinearLayoutManager(applicationContext)
         recyclerView.adapter = adapter
+
+        val swipeToDismissTouchHelper = getSwipeToDismissTouchHelper(adapter)
+        swipeToDismissTouchHelper.attachToRecyclerView(recyclerView)
     }
 
-    private fun createDataList(): List<RowModel> {
-        Log.d("Life Cycle","createDataList")
+    private fun createDataList(): MutableList<RowModel> {
+        Log.d("Life Cycle", "createDataList")
         val dataList = mutableListOf<RowModel>()
         for (i in 0..49) {
             val data: RowModel = RowModel().also {
@@ -42,4 +52,68 @@ class MainActivity : AppCompatActivity() {
         }
         return dataList
     }
+
+    //カードのスワイプアクションの定義
+    private fun getSwipeToDismissTouchHelper(adapter: RecyclerView.Adapter<HomeViewHolder>) =
+        ItemTouchHelper(object : ItemTouchHelper.SimpleCallback(
+            ItemTouchHelper.LEFT or ItemTouchHelper.RIGHT,
+            ItemTouchHelper.LEFT or ItemTouchHelper.RIGHT
+        ) {
+            override fun onMove(
+                recyclerView: RecyclerView,
+                viewHolder: RecyclerView.ViewHolder,
+                target: RecyclerView.ViewHolder
+            ): Boolean {
+                return false
+            }
+
+            //スワイプ時に実行
+            override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
+                //データリストからスワイプしたデータを削除
+                dataList.removeAt(viewHolder.adapterPosition)
+
+                //リストからスワイプしたカードを削除
+                adapter.notifyItemRemoved(viewHolder.adapterPosition)
+            }
+
+            //スワイプした時の背景を設定
+            override fun onChildDraw(
+                c: Canvas,
+                recyclerView: RecyclerView,
+                viewHolder: RecyclerView.ViewHolder,
+                dX: Float,
+                dY: Float,
+                actionState: Int,
+                isCurrentlyActive: Boolean
+            ) {
+                super.onChildDraw(
+                    c,
+                    recyclerView,
+                    viewHolder,
+                    dX,
+                    dY,
+                    actionState,
+                    isCurrentlyActive
+                )
+                val itemView = viewHolder.itemView
+                val background = ColorDrawable()
+                background.color = Color.parseColor("#f44336")
+                if (dX < 0)
+                    background.setBounds(
+                        itemView.right + dX.toInt(),
+                        itemView.top,
+                        itemView.right,
+                        itemView.bottom
+                    )
+                else
+                    background.setBounds(
+                        itemView.left,
+                        itemView.top,
+                        itemView.left + dX.toInt(),
+                        itemView.bottom
+                    )
+
+                background.draw(c)
+            }
+        })
 }
